5a98d6a6ef9bdb27fcb7380b437c12fc16091cc8
= <=>
2019-01-06 04:45:32 -0600

    Alterado CORS e adicionado projeto front-end (Todolist)

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,7 +1,7 @@
 module.exports = function(req,res,next){
 
     res.header('Access-Control-Allow-Origin','*')
-    res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS,PATCH,DELETE');
+    res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS,PATCH,DELETE, PUT');
     res.header('Access-Control-Allow-Headers','X-Requested-With, Content-Type, Accepted');
     
     next();
